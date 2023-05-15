4e89915a6e1e2cff8b1c9ea5f316d77d9a15a9ca
wwwwww3q <phatdatfbi@gmail.com>
2023-05-16 02:16:31 +0700

    remove code from getx

--- lib/app/core/utils/extension/app_extension.dart
+++ lib/app/core/utils/extension/app_extension.dart
@@ -3,6 +3,5 @@ export 'iterable_extension.dart';
 export 'list_extension.dart';
 export 'map_extension.dart';
 export 'num_extension.dart';
-export 'set_extension.dart';
 export 'string_extension.dart';
 export 'text_editing_controller_extension.dart';
--- lib/app/core/utils/extension/list_extension.dart
+++ lib/app/core/utils/extension/list_extension.dart
@@ -16,40 +16,4 @@ extension ListExtension<E> on List<E> {
     // ignore: unnecessary_type_check
     return ((first ~/ second) is int) ? (first ~/ second) as T : (first / second) as T;
   }
-
-  //from GetX
-
-  /// Add [item] to [List<E>] only if [item] is not null.
-  void addNonNull(E item) {
-    if (item != null) add(item);
-  }
-
-  /// Add [Iterable<E>] to [List<E>] only if [Iterable<E>] is not null.
-  // void addAllNonNull(Iterable<E> item) {
-  //   if (item != null) addAll(item);
-  // }
-
-  /// Add [item] to List<E> only if [condition] is true.
-  void addIf(dynamic condition, E item) {
-    if (condition is Condition) condition = condition();
-    if (condition is bool && condition) add(item);
-  }
-
-  /// Adds [Iterable<E>] to [List<E>] only if [condition] is true.
-  void addAllIf(dynamic condition, Iterable<E> items) {
-    if (condition is Condition) condition = condition();
-    if (condition is bool && condition) addAll(items);
-  }
-
-  /// Replaces all existing items of this list with [item]
-  void assign(E item) {
-    clear();
-    add(item);
-  }
-
-  /// Replaces all existing items of this list with [items]
-  void assignAll(Iterable<E> items) {
-    clear();
-    addAll(items);
-  }
 }
